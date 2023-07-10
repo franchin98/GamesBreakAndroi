7bcb5d470bfc7f74e6f52b52c3b63e83171d9e22
Andres Calizaya <andres.u.calizaya@gmail.com>
2023-07-10 00:36:54 -0300

    Refactor home

--- app/src/main/java/com/example/gamesbreak/activities/HomeActivity.kt
+++ app/src/main/java/com/example/gamesbreak/activities/HomeActivity.kt
@@ -1,56 +1,83 @@
 package com.example.gamesbreak.activities
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Observer
 import androidx.lifecycle.asLiveData
-import com.example.gamesbreak.MainActivity
 import com.example.gamesbreak.R
+import com.example.gamesbreak.activities.ProfileHome.ProfileFragment
+import com.example.gamesbreak.activities.StoreHome.StoreFragment
 import com.example.gamesbreak.data.UserCredentials
 import com.example.gamesbreak.data.UserPreferences
 import com.example.gamesbreak.databinding.ActivityHomeBinding
 import com.example.gamesbreak.ui.authentication.AuthenticationActivity
 import com.example.gamesbreak.utils.startNewActivity
-
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
     private var user: UserCredentials? = null
+    private lateinit var fragmentManager: FragmentManager
+    private lateinit var currentFragment: Fragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val storage = UserPreferences(this)
+        fragmentManager = supportFragmentManager
+        showFragment(StoreFragment())
 
+
+        val storage = UserPreferences(this)
         storage.userCredentials.asLiveData().observe(this, Observer { credentials ->
             user = credentials
             if (credentials != null) {
-                binding.tvGreetingUser.text = buildString {
-                    append(getString(R.string.greeting_home))
-                    append(" ${user?.name}!")
+                binding.bottomNavigationView.setOnItemSelectedListener { menuItem ->
+                    when (menuItem.itemId) {
+                        R.id.Store -> {
+                            showFragment(StoreFragment())
+                            true
+                        }
+                        R.id.Profile -> {
+                            showFragment(ProfileFragment())
+                            true
+                        }
+                        else -> false
+                    }
                 }
-                setUpClickListeners()
             } else {
                 startNewActivity(AuthenticationActivity::class.java)
             }
         })
     }
-    private fun setUpClickListeners() {
-        val userId = this.user?.id
-
-        binding.cvMyGames.setOnClickListener {
-            val intentMyGames = Intent(this, GameOfUserActivity::class.java)
-            intentMyGames.putExtra("ID_USER", userId)
-            startActivity(intentMyGames)
-        }
-
-        binding.cvBuyGames.setOnClickListener {
-            val intentGameRecyclerActivity = Intent(this, GameToSaleActivity::class.java)
-            intentGameRecyclerActivity.putExtra("ID_USER", userId)
-            startActivity(intentGameRecyclerActivity)
-        }
-
+    private fun showFragment(fragment: Fragment) {
+        val transaction = fragmentManager.beginTransaction()
+        transaction.replace(R.id.fragmentContainer, fragment)
+        transaction.commit()
+        currentFragment = fragment
     }
 }
+
+
+
+
+//    }
+//    private fun setUpClickListeners() {
+//        val userId = this.user?.id
+//
+//        binding.cvMyGames.setOnClickListener {
+//            val intentMyGames = Intent(this, GameOfUserActivity::class.java)
+//            intentMyGames.putExtra("ID_USER", userId)
+//            startActivity(intentMyGames)
+//        }
+//
+//        binding.cvBuyGames.setOnClickListener {
+//            val intentGameRecyclerActivity = Intent(this, GameToSaleActivity::class.java)
+//            intentGameRecyclerActivity.putExtra("ID_USER", userId)
+//            startActivity(intentGameRecyclerActivity)
+//        }
+//
+//    }
+//}
